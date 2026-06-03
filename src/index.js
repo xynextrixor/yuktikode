f03dd0bed52b7759d3fe1d2160fa52c821210828
anmolsin1124 <anmolsin1124@gmail.com>
2026-06-03 18:36:20 +0530

    SCHEMA

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-require
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+const main = require('./config/db');
+app.use(express.json());
+const cookies = require('cookie-parser');
+
+
+main()
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    })
+    .catch(err => {
+        console.error('Failed to connect to MongoDB', err);
+    });
---
+++ src/models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    firstName: {
+        minLength: 3,
+        type: String,
+        maxLength: 20,
+        required: true
+    },
+    type: String,
+    lastName: {
+        minLength: 3,
+        maxLength: 20,
+        required: true
+    },
+
+    emailId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    age: {
+        type: Number,
+        required: true,
+        min: 9,
+        max: 80
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['admin', 'user']
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    problemsolved: {
+        type: Number,
+        default: 0
+    }
+},
+
+    { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
