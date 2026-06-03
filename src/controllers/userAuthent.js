f58f86e76e3d77af121c8411461d31bd30f32acf
anmolsin1124 <anmolsin1124@gmail.com>
2026-06-03 19:32:23 +0530

    fdsgfdgfd

--- src/controllers/userAuthent.js
+++ src/controllers/userAuthent.js
@@ -1,12 +1,22 @@
 const User = require("../models/user");
 const validate = require("../utils/validator");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 
 const register = async (req, res) => {
     try {
         validate();
         const { firstName, emailId, password } = req.body;
         req.body.password = await bcrypt.hash(password, 10);
+
+
+        jwt.sign({ emailId }, "asdlkjflsakdjf", { expiresIn: 60 * 60 }, (err, token) => {
+            if (err) {
+                throw new Error("Error generating token");
+            }
+            req.body.token = token;
+        });
         const user = await User.create(req.body);
         res.status(201).json({ message: "User registered successfully", user });
     }
@@ -15,3 +25,5 @@ const register = async (req, res) => {
 
     }
 }
+
+///node -e "console.log(require("crypto").randomBytes(32).toString("hex"))"
