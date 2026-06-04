306fadd0493bfbecfc6e7fe7b68156e93effb89c
anmolsin1124 <anmolsin1124@gmail.com>
2026-06-04 11:49:38 +0530

    nhvchg

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express();
 require('dotenv').config();
-const main =  require('./config/db')
-const cookieParser =  require('cookie-parser');
+const main = require('./config/db')
+const cookieParser = require('cookie-parser');
 const authRouter = require("./routes/userAuth");
 const redisClient = require('./config/redis');
 const problemRouter = require("./routes/problemCreator");
@@ -15,32 +15,32 @@ const cors = require('cors')
 
 app.use(cors({
     origin: 'http://localhost:5173',
-    credentials: true 
+    credentials: true
 }))
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/user',authRouter);
-app.use('/problem',problemRouter);
-app.use('/submission',submitRouter);
-app.use('/ai',aiRouter);
-app.use("/video",videoRouter);
+app.use('/user', authRouter);
+app.use('/problem', problemRouter);
+app.use('/submission', submitRouter);
+app.use('/ai', aiRouter);
+app.use("/video", videoRouter);
 
 
-const InitalizeConnection = async ()=>{
-    try{
+const InitalizeConnection = async () => {
+    try {
 
-        await Promise.all([main(),redisClient.connect()]);
+        await Promise.all([main(), redisClient.connect()]);
         console.log("DB Connected");
-        
-        app.listen(process.env.PORT, ()=>{
-            console.log("Server listening at port number: "+ process.env.PORT);
+
+        app.listen(process.env.PORT, () => {
+            console.log("Server listening at port number: " + process.env.PORT);
         })
 
     }
-    catch(err){
-        console.log("Error: "+err);
+    catch (err) {
+        console.log("Error: " + err);
     }
 }
 
