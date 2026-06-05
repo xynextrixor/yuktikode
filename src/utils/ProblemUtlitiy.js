65d93fcc7b1b9a06f80f91993f840e987e7dae62
anmolsin1124 <anmolsin1124@gmail.com>
2026-06-05 17:47:29 +0530

    NOTHI

--- src/utils/ProblemUtlitiy.js
+++ src/utils/ProblemUtlitiy.js
@@ -32,7 +32,7 @@ const submitBatch = (submssion) => {
 async function fetchData() {
     try {
         const response = await axios.request(options);
-        console.log(response.data);
+        return response.data()
     }
     catch (error) {
         console.log(error)
